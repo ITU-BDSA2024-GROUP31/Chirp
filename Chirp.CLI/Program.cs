0e1a0589435b5f3a1b7d9fc8182a85f468064f07
Frederik Andersen <frean@itu.dk>
2024-08-28 15:43:04 +0200

    Added support for reading CSV files and displaying formatted version in console Co-authored-by: Yous113 <Youssef.w.nour@gmail.com> Co-authored-by: Adam-Assi <aass@itu.dk> Co-authored-by: YacobBakiz <yaba@itu.dk> Co-authored-by: DavidFJensen <davj@itu.dk> Co-authored-by: alwaysDebuggging <denw@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+try
+{
+    // Open the text file using a stream reader.
+    using StreamReader reader = new("/Users/frederik/Desktop/Git/Chirp/Chirp.CLI/Data/chirp_cli_db.csv");
+    reader.ReadLine();
+    
+    while (!reader.EndOfStream)
+    {
+        var text = reader.ReadLine();
+        
+        if (text != null)
+        {
+            string[] words = Regex.Split(text, @",(?=(?:[^""]*""[^""]*"")*[^""]*$)");
+            if (words.Length > 2)
+            {
+                var date = DateTimeOffset.FromUnixTimeSeconds(long.Parse(words[2]));
+                var formattedDate = date.ToString("dd/MM/yyyy HH:mm:ss");
+                Console.WriteLine(words[0] + " @ " + formattedDate + ": " + words[1]);
+            }
+            
+        }
+        
+    }
+    
+}
+catch (IOException e)
+{
+    Console.WriteLine("The file could not be read:");
+    Console.WriteLine(e.Message);
+}
+
