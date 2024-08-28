963966f1563fc58af297fcc11e25a6b1020f2dc1
Frederik Andersen <frean@itu.dk>
2024-08-28 15:50:04 +0200

    Changed CSV file path to be relative not absolute so everyone can open it

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,33 +1,36 @@
 ﻿using System.Text.RegularExpressions;
 
-try
+if (args[0] == "read")
 {
-    // Open the text file using a stream reader.
-    using StreamReader reader = new("/Users/frederik/Desktop/Git/Chirp/Chirp.CLI/Data/chirp_cli_db.csv");
-    reader.ReadLine();
-    
-    while (!reader.EndOfStream)
+    try
     {
-        var text = reader.ReadLine();
-        
-        if (text != null)
+        // Open the text file using a stream reader.
+        using StreamReader reader = new("Data/chirp_cli_db.csv");
+        reader.ReadLine();
+
+        while (!reader.EndOfStream)
         {
-            string[] words = Regex.Split(text, @",(?=(?:[^""]*""[^""]*"")*[^""]*$)");
-            if (words.Length > 2)
+            var text = reader.ReadLine();
+
+            if (text != null)
             {
-                var date = DateTimeOffset.FromUnixTimeSeconds(long.Parse(words[2]));
-                var formattedDate = date.ToString("dd/MM/yyyy HH:mm:ss");
-                Console.WriteLine(words[0] + " @ " + formattedDate + ": " + words[1]);
+                string[] words = Regex.Split(text, @",(?=(?:[^""]*""[^""]*"")*[^""]*$)");
+                if (words.Length > 2)
+                {
+                    var date = DateTimeOffset.FromUnixTimeSeconds(long.Parse(words[2]));
+                    var formattedDate = date.ToString("dd/MM/yyyy HH:mm:ss");
+                    Console.WriteLine(words[0] + " @ " + formattedDate + ": " + words[1]);
+                }
+
             }
-            
+
         }
-        
+
     }
-    
-}
-catch (IOException e)
-{
-    Console.WriteLine("The file could not be read:");
-    Console.WriteLine(e.Message);
+    catch (IOException e)
+    {
+        Console.WriteLine("The file could not be read:");
+        Console.WriteLine(e.Message);
+    }
 }
 
