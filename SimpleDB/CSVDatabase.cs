efecbebb3a197c642cacbbe7203970e347e8002e
Yous113 <youssef.w.nour@gmail.com>
2024-09-04 15:44:35 +0200

    Created CSVDatabase class
    Co-authored-by: David <davj@itu.dk>

---
+++ SimpleDB/CSVDatabase.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleDB;
+
+public sealed class CSVDatabase<T> implements IDatabaseRepository
+{
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        
+    }
+
+    public void Store(T record)
+    {
+        
+    }
+}
