0f5722b3443d5fe576f928bb25d0d600c6b046f4
Frederik Andersen <frean@itu.dk>
2024-09-18 15:48:52 +0200

    Created Webservice project and started refactoring of program
    Co-authored-by: Yous113 <Youssef.w.nour@gmail.com>

---
+++ src/Chirp.CSVDBService/Program.cs
@@ -0,0 +1,40 @@
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+app.MapGet("/cheeps", () => Read());
+app.MapPost("/cheep", (Cheep cheep) => cheep);
+
+app.Run();
+
+IEnumerable<Cheep> Read(int? limit = null)
+{
+    using (var reader = new StreamReader("../../data/chirp_cli_db.csv"))
+    using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
+           {
+               HasHeaderRecord = true,
+           }))
+    {
+        var records = limit.HasValue ? csv.GetRecords<Cheep>().Take(limit.Value).ToList() : csv.GetRecords<Cheep>().ToList();
+        return records;
+    }
+}
+
+void Store(Cheep record)
+{
+    using (var writer = new StreamWriter("../../data/chirp_cli_db.csv", append: true))
+    using (var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
+    {
+        csv.WriteRecord(record);
+        writer.WriteLine();
+    }
+}
+
+
+public record Cheep(string Author, string Message, long Timestamp);
+
