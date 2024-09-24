3ef17607260d05715c9162d90a89d40138ff8d92
Dennis <Denniswaweru@live.dk>
2024-09-24 15:06:47 +0200

    Co-authored-by: Yous113 <Youssef.w.nour@gmail.com.dk>
    Co-authored-by: Adam-Assi <aass@itu.dk>
    
    Co-authored-by: YacobBakiz <yaba@itu.dk>
    
    Co-authored-by: DavidFJensen <davj@itu.dk>
    
    Co-authored-by: Co-pilot

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -6,33 +6,15 @@ using System.Globalization;
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+List<Cheep> list = new List<Cheep>();
 
-app.MapGet("/cheeps", () => Read());
-app.MapPost("/cheep", Store);
+app.MapGet("/cheeps", (int? limit) =>
+{
+    var cheeps = limit.HasValue ? list.Take(limit.Value).ToList() : list;
+    return Results.Ok(cheeps);
+});
+app.MapPost("/cheep", (Cheep cheep) => list.Add(cheep));
 
 app.Run();
-
-IEnumerable<Cheep> Read(int? limit = null)
-{
-    using (var reader = new StreamReader("../../data/chirp_cli_db.csv"))
-    using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
-           {
-               HasHeaderRecord = true,
-           }))
-    {
-        var records = limit.HasValue ? csv.GetRecords<Cheep>().Take(limit.Value).ToList() : csv.GetRecords<Cheep>().ToList();
-        return records;
-    }
-}
-
-void Store(Cheep record)
-{
-    using (var writer = new StreamWriter("../../data/chirp_cli_db.csv", append: true))
-    using (var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
-    {
-        csv.WriteRecord(record);
-        writer.WriteLine();
-    }
-}
 
 public record Cheep(string Author, string Message, long Timestamp);
