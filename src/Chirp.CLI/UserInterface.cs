ee3349f6e624ac2343b7b9d4f936d3a094baa402
Yous113 <yono@itu.dk>
2024-09-27 18:51:53 +0200

    Revert "Refactored project and deleted CLI, CSVDBService and SimpleDB"
    This reverts commit a392fc50334d632bd22d1c04f78d4b10739716d4.

---
+++ src/Chirp.CLI/UserInterface.cs
@@ -0,0 +1,13 @@
+class UserInterface
+{
+
+    public static void PrintCheeps(IEnumerable<Cheep> cheeps)
+    {
+        foreach (var cheep in cheeps)
+        {
+            var formattedTime = DateTimeOffset.FromUnixTimeSeconds(cheep.Timestamp).ToString("yyyy-MM-dd HH:mm:ss");
+            Console.WriteLine($"{cheep.Author} @ {formattedTime}: {cheep.Message}");
+        }
+
+    }
+}
---
+++ src/SimpleDB/CSVDatabase.cs
@@ -0,0 +1,43 @@
+﻿namespace SimpleDB;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+public sealed class CsvDatabase<T> : IDatabaseRepository<T>
+{
+    // Private static instance for Singleton pattern
+    private static readonly Lazy<CsvDatabase<T>> _instance = new Lazy<CsvDatabase<T>>(() => new CsvDatabase<T>());
+
+    private readonly string _path;
+
+    // Private constructor to prevent external instantiation
+    private CsvDatabase()
+    {
+        _path = Path.Combine(AppContext.BaseDirectory, "chirp_cli_db.csv");
+    }
+    public static CsvDatabase<T> Instance => _instance.Value;
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        using (var reader = new StreamReader(_path))
+        using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
+               {
+                   HasHeaderRecord = true,
+               }))
+        {
+            var records = limit.HasValue ? csv.GetRecords<T>().Take(limit.Value).ToList() : csv.GetRecords<T>().ToList();
+            return records;
+        }
+    }
+
+    public void Store(T record)
+    {
+        using (var writer = new StreamWriter(_path, append: true))
+        using (var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
+        {
+            csv.WriteRecord(record);
+            writer.WriteLine();
+        }
+    }
+    
+    
+}
