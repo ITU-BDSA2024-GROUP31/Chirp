f2f1f9bcb9c6ca1c488337563e260f7ec4364708
Youssef Noureddine <44925411+Yous113@users.noreply.github.com>
2024-10-23 14:35:40 +0200

    Added identity to project

--- src/Chirp.Razor/Database/ChatDBContext.cs
+++ src/Chirp.Razor/Database/ChatDBContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
 
 namespace Chirp.Razor;
 
-public class ChatDbContext : DbContext
+public class ChatDbContext : IdentityDbContext<Author, IdentityRole<int>, int>
 {
     public DbSet<Author> Authors { get; set; }
     public DbSet<Cheep> Cheeps { get; set; }
--- src/Chirp.Razor/Models/Author.cs
+++ src/Chirp.Razor/Models/Author.cs
@@ -1,20 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Chirp.Razor;
 
-public class Author
+public class Author : IdentityUser<int> // Use int here
 {
-    [Key] 
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
-    public int AuthorId { get; set; }
-    
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int AuthorId { get; set; } 
+
     [StringLength(254)]
     [Required]
-    public string Name{ get; set; }
-    
-    [StringLength(254)]
-    public string? Email{ get; set; }
-    public List<Cheep>? Cheeps { get; set; }
+    public string Name { get; set; }
 
+    [StringLength(254)]
+    public string? Email { get; set; }
+
+    public List<Cheep>? Cheeps { get; set; }
 }
