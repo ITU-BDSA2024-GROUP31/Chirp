513b06dc29ea9934e0cba0bed1596ef532937c50
Dennis <Denniswaweru@live.dk>
2024-10-23 12:27:10 +0200

    Changes after code review

--- src/Chirp.Razor/Database/ChatDBContext.cs
+++ src/Chirp.Razor/Database/ChatDBContext.cs
@@ -11,29 +11,4 @@ public class ChatDbContext : DbContext
     public ChatDbContext(DbContextOptions<ChatDbContext> options) : base(options)
     {
     }
-    
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        // Ensures that author names and email are required and unique
-        base.OnModelCreating(modelBuilder);
-
-        /** modelBuilder.Entity<Author>()
-            .HasIndex(c => c.Name)
-            .IsUnique();
-            **/
-        
-        /**modelBuilder.Entity<Author>()
-            .HasIndex(c => c.Email)
-            .IsUnique();
-        
-        modelBuilder.Entity<Author>()
-            .Property(c => c.Name)
-            .IsRequired();
-        
-        modelBuilder.Entity<Author>()
-            .Property(c => c.Email)
-            .IsRequired(); 
-        **/
-            
-    }
 }
