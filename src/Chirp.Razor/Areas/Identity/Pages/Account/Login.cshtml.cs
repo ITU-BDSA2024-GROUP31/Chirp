75f78a425e7ea931736b87cbbed615a60c18dd84
Youssef Noureddine <44925411+Yous113@users.noreply.github.com>
2024-10-24 19:38:06 +0200

    User can now logout of their account

--- src/Chirp.Razor/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/Chirp.Razor/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -114,8 +114,7 @@ namespace Chirp.Razor.Areas.Identity.Pages.Account
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
-                Console.WriteLine("LoginModel.OnPostAsync: result = " + result);
-                Console.WriteLine(Input.Email + " " + Input.Password + " " + Input.RememberMe);
+
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
