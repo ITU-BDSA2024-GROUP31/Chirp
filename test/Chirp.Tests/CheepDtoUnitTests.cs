dfb21f34f826b7af65c79b59d8d54416e56e945b
denw <denw@itu.dk>
2024-12-05 00:32:15 +0100

    Moved ana added classes
    Added test classes from old main and removed some folders

--- test/Chirp.Tests/CheepDtoUnitTests.cs
+++ test/Chirp.Tests/CheepDtoUnitTests.cs
@@ -7,9 +7,14 @@ public class CheepDtoUnitTests
     [Fact]
     public void TestConvertDateTimeToString()
     {
-        var cheepDto = new CheepDto("author", "message", new DateTime(2021, 1, 1, 12, 0, 0));
+        // Arrange
+        var currentTime = DateTime.Now;
+        var cheepDto = new CheepDto("author", "message", currentTime);
+        
+        // Act
         var result = cheepDto.Timestamp;
+        
         // Assert
-        Assert.Equal("2021-01-01 12:00:00", result);
+        Assert.Equal(currentTime.ToString("yyyy-MM-dd HH.mm.ss"), result);
     }
 }
--- test/Chirp.UI.Tests/PlaywrightTests/PlaywrightSetupTearDownUtil.cs
+++ test/Chirp.UI.Tests/PlaywrightTests/PlaywrightSetupTearDownUtil.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics;
+﻿﻿using System.Diagnostics;
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 
